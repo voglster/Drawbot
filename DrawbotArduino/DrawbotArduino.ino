df2c0162b3f2501582c9cddfed0dcd286a581505
James Vogel <jim.m.vogel@gmail.com>
2016-12-08 00:13:21 -0600

    added demo code for reference, and began rewriting drawbot main

--- DrawbotArduino/DrawbotArduino.ino
+++ DrawbotArduino/DrawbotArduino.ino
@@ -1,13 +1,3 @@
-// Bounce.pde
-// -*- mode: C++ -*-
-//
-// Make a single stepper bounce from one limit to another
-//
-// Copyright (C) 2012 Mike McCauley
-// $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
-
-#include <AccelStepper.h>
-#include <MultiStepper.h>
 #include <Servo.h>
 
 #define LMOT_STEP 2
@@ -23,13 +13,106 @@
 #define STEPS_PER_REVOLUTION 200
 #define MICROSTEPPING 32
 #define PULLY_DIAMETER 24
-#define MM_PER_STEP (1.0 * (PULLY_DIAMETER * PI)/(STEPS_PER_REVOLUTION * MICROSTEPPING))
+#define MM_PER_STEP ((PULLY_DIAMETER * PI)/(STEPS_PER_REVOLUTION * MICROSTEPPING))
 
 #define SERVO_PIN 11
 #define DOWN_ANGLE 0
 #define UP_ANGLE 90
 
-const double HALF_MACHINE_WIDTH = MACHINE_WIDTH / 2.0;
+#define PEN_DOWN 0
+#define PEN_UP 1 
+
+#define HALF_MACHINE_WIDTH (MACHINE_WIDTH / 2.0)
+
+#define COMMAND_BUFFER_SIZE              (20)                                   // What is the longest message Arduino can store?
+// new constants
+
+#define VERSION              (2)                      // firmware version
+#define BAUD                 (115200)                 // How fast is the Arduino talking?(BAUD Rate of Arduino)
+#define SERIAL_BUFFER_SIZE   (64)                                   // What is the longest message Arduino can store?
+#define STEPS_PER_TURN       (STEPS_PER_REVOLUTION * MICROSTEPPING) // depends on your stepper motor.  most are 200.
+#define STEPS_PER_MM         (STEPS_PER_TURN/(PULLY_DIAMETER * PI))                // (400*16)/0.8 with a M5 spindle
+#define MAX_FEEDRATE         (1000000)
+#define MIN_FEEDRATE         (1)
+#define NUM_AXIES            (4)
+
+#define DEBUG 1
+
+typedef struct {
+  long delta;  // number of steps to move
+  long absdelta;
+  long over;  // for dx/dy bresenham calculations
+} Axis;
+
+typedef struct {
+  long left_steps;
+  long right_steps;
+  byte pen_state;
+  long feed_rate;
+  long delay_ms;
+} Command;
+
+Command commands[COMMAND_BUFFER_SIZE];
+
+byte current_command_index = 0;
+byte next_command_index = 1;
+
+char serial_buffer[SERIAL_BUFFER_SIZE];  // where we store the message until we get a ';'
+byte serial_buffer_index;  // how much is in the buffer
+
+void clear_command_buffer(){
+  for(int i=0; i < COMMAND_BUFFER; i++){
+    commands[i].left_steps  = 0;
+    commands[i].right_steps = 0;
+    commands[i].pen_state   = PEN_DOWN;
+    commands[i].feed_rate   = 0;
+    commands[i].delay_ms    = 0;
+  }
+  location_index = 0;
+  next_command_index = 1;
+}
+
+int next_command_buffer_index(int last_index){
+  last_index++;
+  if(last_index == COMMAND_BUFFER_SIZE)
+    last_index = 0;
+  return last_index;
+}
+
+bool another_command_ready(){
+  return !(next_command_buffer_index(current_command_index) == next_command_index);
+}
+
+bool is_buffer_full(){
+  return !(next_command_buffer_index(next_command_index) == current_command_index);
+}
+
+void ProcessSerial(){
+  if(Serial.available() > 0) {  // if something is available
+    char c=Serial.read();  // get it
+#ifdef DEBUG
+    Serial.print(c);  // repeat it back so I know you got the message
+#endif
+    if(serial_buffer_index<SERIAL_BUFFER_SIZE-1) serial_buffer[serial_buffer_index++]=c;  // store it
+    if(c=='\n') {
+      // entire message received
+      serial_buffer[serial_buffer_index]=0;  // end the buffer so string functions work right
+#ifdef DEBUG
+      Serial.print(F("\r\n"));  // echo a return character for humans
+#endif
+      processCommand();  // do something with the command
+      ready();
+    }
+  }
+}
+
+void ready(){
+  while(!is_buffer_full())
+    Serial.println(F("Ready"));  // echo a return character for humans
+}
+
+
+
 double last_x = 0;
 double last_y = 0;
 double lastSpeed = 0;
@@ -40,21 +123,14 @@ boolean moving = false;
 
 long positions[2];
 
-// Define a stepper and the pins it will use
-AccelStepper lmot(AccelStepper::DRIVER, LMOT_STEP, LMOT_DIR);
-AccelStepper rmot(AccelStepper::DRIVER, RMOT_STEP, RMOT_DIR);
-
-//MultiStepper
-MultiStepper steppers;
-
 Servo penServo;
 
 boolean moveComplete = true;
 
 void setup()
 {
-	setupSteppers();
-	penServo.attach(SERVO_PIN);
+	clear_command_buffer();
+  penServo.attach(SERVO_PIN);
 	Serial.begin(115200);
 	penDown();
 	penUp();
@@ -65,56 +141,9 @@ void setup()
 	Serial.println("Ready");
 }
 
-void penUp() {
-	penServo.write(UP_ANGLE);
-	delay(500);
-}
-
-void penDown() {
-	penServo.write(DOWN_ANGLE);
-	delay(500);
-}
-
 void loop()
 {
-	if (moveComplete && commandReady) {
-		handleCommand(command);
-	}
-	if (moveComplete && moving) {
-		moving = false;
-		//showCurrentPosition();
-		Serial.println("Ok");
-	}
-
-	moveComplete = !steppers.run();
-}
-
-void setSpeed(float mm) {
-	lastSpeed = mm;
-	lmot.setMaxSpeed((long)(mm / MM_PER_STEP));
-	rmot.setMaxSpeed((long)(mm / MM_PER_STEP));
-}
-
-void setupSteppers() {
-	lmot.setEnablePin(ENA_PIN);
-	lmot.setPinsInverted(false, false, true);
-	setSpeed(MAX_MOT_SPEED);
-	steppers.addStepper(lmot);
-	steppers.addStepper(rmot);
-	copyMotorPositions();
-}
-
-void copyMotorPositions() {
-	positions[0] = lmot.currentPosition();
-	positions[1] = rmot.currentPosition();
-}
-
-void gotoMM(double lmm, double rmm) {
-	positions[0] = convertMMtoSteps(lmm);
-	positions[1] = convertMMtoSteps(rmm);
-	steppers.moveTo(positions);
-	moving = true;
-	moveComplete = !steppers.run();
+  ProcessSerial();
 }
 
 void gotoXY(double x, double y) {
@@ -125,86 +154,12 @@ void gotoXY(double x, double y) {
 	gotoMM(ll, rl);
 }
 
-
-
-void warp(double x, double y) {
-	last_x = x;
-	last_y = y;
-	y += (double)VERTICAL_OFFSET;
-	if (x == 0) {
-		if (y == 0) {
-			//horizontal... should never happen?
-			long steps = convertMMtoSteps(HALF_MACHINE_WIDTH);
-			setPosition(steps, steps);
-			return;
-		}
-		//on centerline... both left and right are the same
-		double mm = pythag(HALF_MACHINE_WIDTH, y);
-		long steps = convertMMtoSteps(mm);
-		setPosition(steps, steps);
-		return;
-	}
-	if (y == 0) {
-		setPosition(convertMMtoSteps(HALF_MACHINE_WIDTH + x), convertMMtoSteps(HALF_MACHINE_WIDTH - x));
-		return;
-	}
-	//x and y are not zero have to calc
-	double ll = pythag(HALF_MACHINE_WIDTH + x, y);
-	double rl = pythag(HALF_MACHINE_WIDTH - x, y);
-	setPosition(convertMMtoSteps(ll), convertMMtoSteps(rl));
-}
-
-double pythag(double x, double y) {
-	return sqrt(sq(x) + sq(y));
-}
-
-void setPosition(long ls, long rs) {
-	lmot.setCurrentPosition(ls);
-	rmot.setCurrentPosition(rs);
-}
-
-void enableSteppers() {
-	lmot.enableOutputs();
-}
-
-void disableSteppers() {
-	lmot.disableOutputs();
-}
-
-void showCurrentPosition() {
-	Serial.print(F("Current step position "));
-	Serial.print(lmot.currentPosition());
-	Serial.print(F(","));
-	Serial.println(rmot.currentPosition());
-}
-
-void showTargetPosition() {
-	Serial.print(F("Target step position "));
-	Serial.print(lmot.targetPosition());
-	Serial.print(F(","));
-	Serial.println(rmot.targetPosition());
-}
-
-void showMaxSpeeds() {
-	Serial.print(F("Current speeds "));
-	Serial.print(lmot.maxSpeed());
-	Serial.print(F(","));
-	Serial.println(rmot.maxSpeed());
-}
-
-void serialEvent() {
-	while (Serial.available()) {
-		char inChar = (char)Serial.read();
-		command += inChar;
-		if (inChar == '\n') {
-			command.toUpperCase();
-			int idx = command.indexOf(' ');
-			while (idx != -1) {
-				command.remove(idx);
-			}
-			commandReady = true;
-		}
-	}
+void gotoMM(double lmm, double rmm) {
+	positions[0] = convertMMtoSteps(lmm);
+	positions[1] = convertMMtoSteps(rmm);
+	steppers.moveTo(positions);
+	moving = true;
+	moveComplete = !steppers.run();
 }
 
 long convertMMtoSteps(double mm) {
@@ -215,177 +170,7 @@ double convertStepstoMM(long steps) {
 	return (double)(MM_PER_STEP * steps);
 }
 
-void handleCommand(String &command) {
-	commandReady = false;
-	parseCommand(command);
-	command = "";
+double pythag(double x, double y) {
+	return sqrt(sq(x) + sq(y));
 }
 
-void parseCommand(String &command) {
-	command.toUpperCase();
-	if (command.charAt(0) == 'G')
-		parseGCommand(command);
-	else
-		parseMCommand(command);
-}
-
-void parseMCommand(String &com) {
-	if (com.charAt(1) == '1' and com.charAt(2) == '7') {
-		enableSteppers();
-		Serial.println("Ok");
-	}
-	else if (com.charAt(1) == '1' and com.charAt(2) == '8') {
-		disableSteppers();
-		Serial.println("Ok");
-	}
-	else if (com.charAt(1) == '1' and com.charAt(2) == '1' and com.charAt(3) == '4') {
-		Serial.print("X");
-		Serial.print(last_x);
-		Serial.print("Y");
-		Serial.print(last_y);
-		Serial.print("F");
-		Serial.println(lastSpeed);
-		Serial.println("Ok");
-	}
-	else if (com.charAt(1) == '3' or com.charAt(1) == '4') {
-		penDown();
-		Serial.println("Ok");
-	}
-	else if (com.charAt(1) == '5') {
-		penUp();
-		Serial.println("Ok");
-	}
-	else {
-		Serial.println("Err Unknown Command" + com);
-	}
-}
-
-void parseGCommand(String &com) {
-	if (com.charAt(1) == '1' or com.charAt(1) == '0') {
-		float tgt_x = -9999;
-		float tgt_y = -9999;
-		float tgt_f = -9999;
-
-		int xidx = com.indexOf('X');
-		int yidx = com.indexOf('Y');
-		int fidx = com.indexOf('F');
-		if (xidx != -1) {
-			//we have a x index
-			if (yidx == -1 and fidx == -1) {
-				//only x just read it in
-				tgt_x = com.substring(xidx + 1).toFloat();
-			}
-			else {
-				if (yidx == -1) {
-					//no y but there must be a f
-					if (fidx < xidx) {
-						//f before x
-						tgt_f = com.substring(fidx + 1, xidx).toFloat();
-						tgt_x = com.substring(xidx + 1).toFloat();
-					}
-					else {
-						//x before f
-						tgt_x = com.substring(xidx + 1, fidx).toFloat();
-						tgt_f = com.substring(fidx + 1).toFloat();
-					}
-				}
-				else if (fidx == -1) {
-					//x and y defined, missing f
-					if (yidx < xidx) {
-						//y before x
-						tgt_y = com.substring(yidx + 1, xidx).toFloat();
-						tgt_x = com.substring(xidx + 1).toFloat();
-					}
-					else {
-						//x before y
-						tgt_x = com.substring(xidx + 1, fidx).toFloat();
-						tgt_y = com.substring(yidx + 1).toFloat();
-					}
-				}
-				else {
-					//all defined lets get order
-					//{f,y,x}
-					if (xidx < yidx and yidx < fidx) {
-						//order x,y,f
-						tgt_x = com.substring(xidx + 1, yidx).toFloat();
-						tgt_y = com.substring(yidx + 1, fidx).toFloat();
-						tgt_f = com.substring(fidx + 1).toFloat();
-					}
-					else if (yidx < xidx and xidx < fidx) {
-						//order y,x,f
-						tgt_y = com.substring(yidx + 1, xidx).toFloat();
-						tgt_x = com.substring(xidx + 1, fidx).toFloat();
-						tgt_f = com.substring(fidx + 1).toFloat();
-					}
-					else if (yidx < fidx and fidx < xidx) {
-						//order y,f,x
-						tgt_y = com.substring(yidx + 1, fidx).toFloat();
-						tgt_f = com.substring(fidx + 1, xidx).toFloat();
-						tgt_x = com.substring(xidx + 1).toFloat();
-					}
-					else if (xidx < fidx and fidx < yidx) {
-						//order x,f,y
-						tgt_x = com.substring(xidx + 1, fidx).toFloat();
-						tgt_f = com.substring(fidx + 1, yidx).toFloat();
-						tgt_y = com.substring(yidx + 1).toFloat();
-					}
-					else if (fidx < xidx and xidx < yidx) {
-						//order f,x,y
-						tgt_f = com.substring(fidx + 1, xidx).toFloat();
-						tgt_x = com.substring(xidx + 1, yidx).toFloat();
-						tgt_y = com.substring(yidx + 1).toFloat();
-					}
-					else {
-						//order f,y,x
-						tgt_f = com.substring(fidx + 1, yidx).toFloat();
-						tgt_y = com.substring(yidx + 1, xidx).toFloat();
-						tgt_x = com.substring(xidx + 1).toFloat();
-					}
-				}
-
-			}
-		}
-		else {
-			//x not defined
-			if (fidx == -1) {
-				//only y defined
-				tgt_y = com.substring(yidx + 1).toFloat();
-			}
-			else if (yidx == -1) {
-				//only f defined
-				tgt_f = com.substring(fidx + 1).toFloat();
-			}
-			else {
-				//f and y defined
-				if (fidx < yidx) {
-					tgt_f = com.substring(fidx + 1, yidx).toFloat();
-					tgt_y = com.substring(yidx + 1).toFloat();
-				}
-				else {
-					tgt_y = com.substring(yidx + 1, fidx).toFloat();
-					tgt_y = com.substring(fidx + 1).toFloat();
-				}
-			}
-		}
-		if (tgt_f != -9999)
-			setSpeed(tgt_f);
-		if (tgt_x == -9999) {
-			tgt_x = last_x;
-		}
-		if (tgt_y == -9999) {
-			tgt_y = last_y;
-		}
-		gotoXY(tgt_x, tgt_y);
-
-	}
-	else {
-		if (com.charAt(1) == '9' and com.charAt(2) == '2') {
-			warp(0, 0);
-			Serial.println(F("Ok"));
-		}
-		else {
-			Serial.print(F("Err Unknown Command"));
-			Serial.println(com);
-		}
-	}
-}
